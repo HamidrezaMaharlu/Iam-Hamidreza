a2404b452b33c5fdf3467606ed12f3c886b6c44b
HamidrezaMaharlu <maharlu.hamidreza@gmail.com>
2020-08-16 00:35:03 +0430

    Updates

---
+++ precache-manifest.0ac72d4c10326c7713329111a104b436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2333f36ca2c5b117ef589661e59ced79",
+    "url": "/Iam-Hamidreza/index.html"
+  },
+  {
+    "revision": "8d7b87d061c958069fef",
+    "url": "/Iam-Hamidreza/static/css/main.69c9c305.chunk.css"
+  },
+  {
+    "revision": "f6fa6735080753d0e5d2",
+    "url": "/Iam-Hamidreza/static/js/2.4fda0bae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Iam-Hamidreza/static/js/2.4fda0bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7b87d061c958069fef",
+    "url": "/Iam-Hamidreza/static/js/main.6a96f670.chunk.js"
+  },
+  {
+    "revision": "f4ebc59872a01e2a7120a6b154d3c867",
+    "url": "/Iam-Hamidreza/static/js/main.6a96f670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98ba09966b00e6c7b4d",
+    "url": "/Iam-Hamidreza/static/js/runtime-main.c0c9983d.js"
+  }
+]);
